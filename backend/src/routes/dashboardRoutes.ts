a124ab54a2420660ef066bb6a5b5add473231116
Vaibhav Kapadiya <kapadiyavaibhav01@gmail.com>
2026-05-19 10:38:27 +0530

    Update dashboardRoutes.ts

--- backend/src/routes/dashboardRoutes.ts
+++ backend/src/routes/dashboardRoutes.ts
@@ -1,9 +1,8 @@
-import express from "express";
+import { Router } from "express";
 import { getDashboardStats } from "../controllers/dashboardController";
 import { protect } from "../middleware/authMiddleware";
 
-const router = express.Router();
-
+const router = Router();
 router.get("/",protect,getDashboardStats);
 
 export default router;
